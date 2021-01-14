5b368ac6f25ce7a1057142968ebae30dc3ce3bfb
Muhammad Sumon Molla Selim <sumonmselim@gmail.com>
2021-01-14 23:23:54 +0600

    📦 NEW: #7 Configure medialibrary for media management

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Tweet extends Model
+class Tweet extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     /**
      * The attributes that are not mass assignable.
@@ -29,5 +31,10 @@ class Tweet extends Model
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('tweet_media');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Log;
+use Spatie\Image\Exceptions\InvalidManipulation;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class User extends Authenticatable
+class User extends Authenticatable implements HasMedia
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, InteractsWithMedia;
 
     /**
      * The attributes that are not mass assignable.
@@ -36,4 +41,20 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'date_of_birth' => 'date',
     ];
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('profile_photo');
+    }
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        try {
+            $this->addMediaConversion('thumbnail')
+                ->width(128)
+                ->height(128);
+        } catch (InvalidManipulation $e) {
+            Log::error('User registerMediaConversions: '.$e->getMessage());
+        }
+    }
 }
